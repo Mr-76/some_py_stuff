93f8da208fe785f5df235c2e60a79b835028df15
Vitor <67565234+Mr-76@users.noreply.github.com>
2022-02-16 16:24:54 -0300

    Add files via upload

---
+++ soma_1.py
@@ -0,0 +1,12 @@
+
+valor = 0;
+
+
+def soma_1(valor):
+    valor = valor +1
+    return valor
+
+
+
+
+
---
+++ test_sample.py
@@ -0,0 +1,8 @@
+from soma_1 import *
+
+
+
+class TestClass:
+    def test_soma(self):
+        assert soma_1(2) == 2
+
