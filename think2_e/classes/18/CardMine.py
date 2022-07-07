76a580dbc97d9da2932a8f60fb46a73f6ac27057
Mr-76 <thegreath76@gmail.com>
2022-07-07 12:39:39 -0300

    chap 18 start

---
+++ think2_e/classes/18/CardMine.py
@@ -0,0 +1,22 @@
+
+class Card:
+    """Represents a standard playing card.
+    
+    Attributes:
+      suit: integer 0-3
+      rank: integer 1-13
+    """
+
+    suit_names = ["Clubs", "Diamonds", "Hearts", "Spades"]
+    rank_names = [None, "Ace", "2", "3", "4", "5", "6", "7", 
+              "8", "9", "10", "Jack", "Queen", "King"]
+
+    def __init__(self, suit=0, rank=2):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        """Returns a human-readable string representation."""
+        return '%s of %s' % (Card.rank_names[self.rank],
+                             Card.suit_names[self.suit])
+#comparing cards
