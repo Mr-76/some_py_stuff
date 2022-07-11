645483719beae2674197c2883ef37738d320f7fc
Mr-76 <thegreath76@gmail.com>
2022-07-11 15:10:03 -0300

    new code

--- think2_e/classes/18/CardMine.py
+++ think2_e/classes/18/CardMine.py
@@ -1,4 +1,4 @@
-
+import random
 class Card:
     """Represents a standard playing card.
     
@@ -19,4 +19,34 @@ class Card:
         """Returns a human-readable string representation."""
         return '%s of %s' % (Card.rank_names[self.rank],
                              Card.suit_names[self.suit])
-#comparing cards
+    def __lt__(self,other):
+        t1 = (self.suit,self.rank)
+        t2 = (other.suit,other.rank)
+        return (t1 < t2)
+class Deck:
+    def __init__(self):
+        self.cards = []
+        for suit in range(4):
+            for rank in range(1,14):
+                card = Card(suit,rank)
+                self.cards.append(card)
+    def __str__(self):
+        res = []
+        for card in self.cards:
+            res.append(str(card))
+        return '\n'.join(res)
+    def pop_card(self):
+        return self.cards.pop()
+    def add_card(self,card):
+        self.cards.append(card)
+    def shuffe(self):
+        random.shuffle(self.cards)
+class Hand(Deck):
+    """rep a had playing cards"""
+    def __init__(self,label=""):
+        self.cards = []
+        self.label = label
+    def move_cards(self, hand, num):
+        for i in range(num):
+            hand.add_card(self.pop_card())
+            #card in the end
